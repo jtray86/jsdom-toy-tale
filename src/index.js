469917e2ce57a6c0b29d7868aa280c0741709964
jennifer tracy <jenniferetracy@gmail.com>
2021-02-08 12:01:44 -0500

    3 deliverables compleate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,75 @@
+// Globle Variables //
 let addToy = false;
+let URL = `http://localhost:3000/toys` 
 
 document.addEventListener("DOMContentLoaded", () => {
+  
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector("#toy-collection");
+  const toyForm = document.querySelector('.add-toy-form')
+
+// fetch // name toy
+function fetchToys(){
+  fetch(URL)
+  .then(response => response.json())
+  .then(toysData => toysData.forEach(toy => renderToyCard(toy)))
+}
+
+
+fetchToys()
+
+  // Rnders//
+
+  function renderToyCard(toy){
+    console.log(toy)
+    let toyName = toy.name
+    let toyImage = toy.image
+    let toyLikes = toy.likes
+
+    const cardDiv = document.createElement('div')
+    cardDiv.setAttribute("class", "card")
+    const cardName = document.createElement('h2')
+    const cardImg = document.createElement('img')
+    cardImg.setAttribute("src", toyImage)
+    cardImg.setAttribute("class", "toy-avatar")
+
+    const cardlikes = document.createElement('p')
+    const cardButton = document.createElement('button')
+    cardButton.setAttribute("class", "like-btn")
+
+    cardName.innerText = toyName
+    cardlikes.innerText = `${toyLikes} Likes`
+    cardButton.innerText = "Like"
+
+    cardDiv.append(cardName, cardImg, cardlikes, cardButton)
+    toyCollection.appendChild(cardDiv)  
+
+  }
+
+  function formInput(e){
+    e.preventDefault()
+    let toyName = e.target.name.value
+    let toyImage = e.target.image.value
+    fetch(URL, {
+      method: "POST",
+      "headers": {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "name": toyName,
+        "image": toyImage,
+        "likes": 0
+      })
+    })
+    .then(response => response.json())
+    .then(toysData => toysData.forEach(toy => renderToyCard(toy)))
+  }
+
+  
+
+  // Event //
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +79,11 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  toyForm.addEventListener("submit", formInput) 
+
 });
+
+
+
+
